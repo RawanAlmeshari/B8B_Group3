1011d3608859e14cf198ff87b21d2080c31f3dfa
Rawan_Almeshari <rksaa@outlook.com>
2022-11-04 19:05:30 +0300

    Generate Report

--- src/pkg251/group/Main.java
+++ src/pkg251/group/Main.java
@@ -1,4 +1,5 @@
 package pkg251.group;
+
 import java.util.*;
 import java.io.*;
 import java.text.ParseException;
@@ -14,7 +15,7 @@ public class Main {
     static ArrayList<Worker> worker;
     static Scanner input = new Scanner(System.in);
     static Scanner input2 = new Scanner(System.in);
-
+    
     public static void main(String[] args) throws FileNotFoundException, ParseException, IOException {
         customer = storeCustomer(new File("customers.txt"));
         worker = new ArrayList<>();
@@ -24,22 +25,21 @@ public class Main {
         storeServices(new File("Services.txt"));
         appointment = new ArrayList<>();
         storeAppointment(new File("appointment.txt"));
-
+        
         int choice = 0;
         do {
             menu();
             choice = input2.nextInt();
-
+            
             if (choice == 1) {
                 Book();
             } else if (choice == 2) {
                 cancelAppointmrnt();
             } else if (choice == 3) {
                 
-
             } else if (choice == 4) {
-                
-
+                generateReport();
+                System.exit(0);
             } else if (choice == 5) {
                 int show = 0;
                 do {
@@ -56,23 +56,23 @@ public class Main {
                             if (!manager.get(i).getName().isEmpty()) {
                                 System.out.println(manager.get(i).toString(manager.get(i)));
                             }
-
+                            
                         }
-
+                        
                     } else if (show == 3) {
                         for (int i = 0; i < worker.size(); i++) {
                             if (!worker.get(i).getName().isEmpty()) {
                                 System.out.println(worker.get(i).toString(worker.get(i)));
                             }
-
+                            
                         }
-
+                        
                     } else if (show == 4) {
                         for (int i = 0; i < service.size(); i++) {
                             if (!service.get(i).getName().isEmpty()) {
                                 System.out.println(service.get(i).toString(service.get(i)));
                             }
-
+                            
                         }
                     } else if (show == 5) {
                         for (int i = 0; i < appointment.size(); i++) {
@@ -86,16 +86,16 @@ public class Main {
                         System.out.print("Wronge Choice, Enter your choice again:");
                         show = input2.nextInt();
                     }
-
+                    
                 } while (true);
-
+                
             } else if (choice == 6) {
                 System.exit(0);
             } else {
                 System.out.print("Wronge Choice, Enter your choice again:");
                 choice = input2.nextInt();
             }
-
+            
         } while (true);
     }
 
@@ -112,7 +112,7 @@ public class Main {
             int serviceID = input3.nextInt();
             while (serviceID == -1) {
                 System.out.println("choose number from the menu pleas :)");
-
+                
                 servicesMenu();
                 serviceID = input3.nextInt();
             }
@@ -124,7 +124,7 @@ public class Main {
                 date = input3.next();
             }
             Appointment thisAppointment = new Appointment(Integer.toString(appointmentsID), choosenservise, customer.get(doesExist), date);
-
+            
             appointment.add(thisAppointment);
             thisAppointment.wirteOnFile(thisAppointment.fileFormat());
             appointmentsID++;
@@ -138,31 +138,30 @@ public class Main {
         System.out.print("what is your phone number? ");
         String number = input2.next();
         int check = searchPhoneNum(number);
-        if(check !=-1){
-        String appAssign=appointmentsAssign(number);
-        System.out.println("\n\n"+appAssign);
-        if (!appAssign.equalsIgnoreCase("sorry there is no appointemnst assign to this number")){
-        System.out.print("What is teh ID of appointment you want to canceal? ");
-        String id = input2.next();
-        int index = searchID(id);
-        if (index == -1) {
-            System.out.println("\nThere is no appointment with id=" + id + " in the system");
+        if (check != -1) {
+            String appAssign = appointmentsAssign(number);
+            System.out.println("\n\n" + appAssign);
+            if (!appAssign.equalsIgnoreCase("sorry there is no appointemnst assign to this number")) {
+                System.out.print("What is teh ID of appointment you want to canceal? ");
+                String id = input2.next();
+                int index = searchID(id);
+                if (index == -1) {
+                    System.out.println("\nThere is no appointment with id=" + id + " in the system");
+                } else {
+                    Appointment.removeFromFile(String.valueOf(id));
+                    appointment.remove(appointment.get(index));
+                    System.out.println("\nthe appointment has been deleted.");
+                }
+            }
         } else {
-            Appointment.removeFromFile(String.valueOf(id));
-            appointment.remove(appointment.get(index));
-            System.out.println("\nthe appointment has been deleted.");
-        }
-        }
-        }
-        else{
             System.out.println("\n\n there's not recored for this number sorry");
         }
-
+        
     }
 
     //menu for show details 
     public static void showOptions() {
-
+        
         System.out.print("1. Show customers.\n"
                 + "2. show Managers.\n"
                 + "3. show workers.\n"
@@ -170,12 +169,12 @@ public class Main {
                 + "5. show appointments.\n"
                 + "6. Back to menu.\n"
                 + "What do you want to show: ");
-
+        
     }
 
     // service menu 
     public static void servicesMenu() {
-
+        
         System.out.print("1.hairCut \n"
                 + "2.Pedicure  \n"
                 + "3.manicure\n "
@@ -183,7 +182,7 @@ public class Main {
                 + "5.Wax \n"
                 + "6.hairDry \n"
                 + "Your Choice: ");
-
+        
     }
 
     // main menu 
@@ -198,21 +197,21 @@ public class Main {
                 + "5. show details\n"
                 + "6. Exit. \n"
                 + "Your Choice: ");
-
+        
     }
 
     //stroe employee from file to arraylists depend on wheter they are worker or manger
     public static void storeEmployee(File file) throws FileNotFoundException {
-
+        
         input = new Scanner(file);
-
+        
         if (file.exists()) {
             while (input.hasNext()) {
                 String type = input.next();
                 if (type.equalsIgnoreCase("manager")) {
                     manager.add(new Manager(input.next(), input.next(), input.nextInt(), input.next(),
                             input.next(), input.nextDouble()));
-
+                    
                 } else {
                     worker.add(new Worker(input.next(), input.next(), input.nextInt(), input.next(),
                             input.next(), input.nextDouble()));
@@ -222,7 +221,7 @@ public class Main {
             }
         }
     }
-    
+
     //store servcies from file to array list
     public static void storeServices(File file) throws FileNotFoundException {
         input = new Scanner(file);
@@ -239,18 +238,18 @@ public class Main {
                 }
             }
         }
-
+        
     }
 
     //store Customers from file to array list 
     public static ArrayList<Customer> storeCustomer(File file) throws FileNotFoundException {
         input = new Scanner(file);
         ArrayList<Customer> customer = new ArrayList<>();
-
+        
         if (file.exists()) {
             while (input.hasNext()) {
                 customer.add(new Customer(input.next(), input.next(), input.nextInt(), input.next(), input.next()));
-
+                
             }
         }
         return customer;
@@ -274,11 +273,11 @@ public class Main {
                 } else {
                     appointment.add(new Appointment(id, service.get(index), customer.get(index2), input.next()));
                 }
-
+                
             }
             appointmentsID = Integer.parseInt(id) + 1;
         }
-
+        
     }
 
     //search in worker arraylist for worker name and return its index if exists else -1
@@ -295,7 +294,7 @@ public class Main {
 
     //search in customer arraylist for phone number and return its index if exists else -1
     public static int searchPhoneNum(String phone) {
-
+        
         for (int i = 0; i < customer.size(); i++) {
             if (!customer.get(i).getPhoneNum().isEmpty()) {
                 if (customer.get(i).getPhoneNum().equalsIgnoreCase(phone)) {
@@ -317,7 +316,7 @@ public class Main {
         }
         return -1;
     }
-
+    
     public static int searchID(String id) {
         for (int i = 0; i < appointment.size(); i++) {
             if (!appointment.get(i).getId().isEmpty()) {
@@ -325,26 +324,56 @@ public class Main {
                     return i;
                 }
             }
-
+            
         }
         return -1;
     }
-        public static String appointmentsAssign(String phone) {
-         String app="";
+    
+    public static String appointmentsAssign(String phone) {
+        String app = "";
         for (int i = 0; i < appointment.size(); i++) {
             if (!appointment.get(i).getCustomer().getPhoneNum().isEmpty()) {
                 if (appointment.get(i).getCustomer().getPhoneNum().equalsIgnoreCase(phone)) {
-                    app+=appointment.get(i).fileFormat()+"\n";
+                    app += appointment.get(i).fileFormat() + "\n";
                 }
             }
         }
-        if(!app.isEmpty()){
-            return app; 
+        if (!app.isEmpty()) {
+            return app;
         }
         return "sorry there is no appointemnst assign to this number";
     }
-        
     
-
+    public static void generateReport() {
+        System.out.println("============================================");
+        System.out.println("\tWeekly Report For Louts Salon\n"
+                + "============================================");
+        System.out.println("\nManager Of the Salon: " + manager.get(0).getName() + "\n");
+        System.out.println("Total Appointment: " + appointment.size());
+        dashes();
+        System.out.println("    Service\t\t Price\t\t Worker");
+        dashes();
+        for (int i = 0; i < service.size(); i++) {
+            if (!service.get(i).getName().isEmpty()) {
+                System.out.println((i + 1) + ".  " + service.get(i).getName() + "\t\t" + service.get(i).getPrice() + "SAR\t\t" + service.get(i).getDoneBy().getName());
+            }
+        }
+        dashes();
+        Report report = new Report();
+        System.out.println("");
+        dashes();
+        System.out.println("\tService\t\t Reservation\t\t Price\t\t");
+        dashes();
+        for (int i = 0; i < service.size(); i++) {
+            System.out.println(service.get(i).getName() + "\t\t"+ report.calculateServices(appointment, service.get(i).getName())+"\t\t"+report.calculatePrice(appointment, i, service)+"SAR");
+        }
+        System.out.println("");
+        dashes();
+        System.out.println("Total income form Appointments: " + report.calculateIncome(appointment) +"SAR");
+        dashes();
+    }
+    
+    public static void dashes() {
+        System.out.println("--------------------------------------------------------");
+    }
 }
-
--- src/pkg251/group/Report.java
+++ src/pkg251/group/Report.java
@@ -1,10 +1,40 @@
-
 package pkg251.group;
 
-public class Report extends Manager{
-    
-    public void generateReport(WorkSchedule schedule, Appointment appointment){
-        
+import java.util.ArrayList;
+
+public class Report extends Manager {
+
+    ArrayList<Appointment> appointment;
+    ArrayList<Service> service;
+
+    public void generateReport(WorkSchedule schedule, Appointment appointment) {
+
     }
-    
+
+    public void Report(ArrayList<Appointment> appointment) {
+        this.appointment = appointment;
+
+    }
+
+    public int calculateIncome(ArrayList<Appointment> appointment) {
+        int sum = 0;
+        for (int i = 0; i < appointment.size(); i++) {
+            sum += appointment.get(i).getChosenService().getPrice();
+        }
+        return sum;
+    }
+
+    public int calculateServices(ArrayList<Appointment> appointment, String service) {
+        int ServiceNum = 0;
+        for (int i = 0; i < appointment.size(); i++) {
+            if (appointment.get(i).getChosenService().getName().equalsIgnoreCase(service)) {
+                ServiceNum += 1;
+            } 
+        }
+        return ServiceNum;
+    }
+
+    public double calculatePrice(ArrayList<Appointment> appointment, int i, ArrayList<Service> service) {
+        return service.get(i).getPrice() * calculateServices(appointment, service.get(i).getName());
+    }
 }
